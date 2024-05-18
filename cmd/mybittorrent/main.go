80b6b0fa1c8acc5d802034c575ea57f871dd4646
Otavio Migliavacca Madalosso <otaviomadalosso@gmail.com>
2024-05-18 16:22:04 -0300

    get peed id

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -128,7 +128,7 @@ func main() {
 		fmt.Printf("Length: %v\n", torrent.Length)
 		fmt.Printf("Info Hash: %s\n", hex.EncodeToString(torrent.InfoHash[:]))
 		fmt.Printf("Piece Length: %d\n", torrent.PieceLength)
-		for i:=0; i< len(torrent.PieceHashes); i++{
+		for i := 0; i < len(torrent.PieceHashes); i++ {
 			fmt.Printf("%s\n", hex.EncodeToString(torrent.PieceHashes[i][:]))
 
 		}
@@ -140,7 +140,8 @@ func main() {
 
 		q := req.URL.Query()
 		q.Add("info_hash", string(torrent.InfoHash[:]))
-		q.Add("peer_id", "06225127954136140002")
+		// q.Add("peer_id", "06225127954136140002")
+		q.Add("peer_id", "00112233445566778899")
 		q.Add("port", "6881")
 		q.Add("uploaded", "0")
 		q.Add("downloaded", "0")
@@ -155,18 +156,45 @@ func main() {
 			os.Exit(1)
 		}
 
-		responseData , _ := io.ReadAll(response.Body)
+		responseData, _ := io.ReadAll(response.Body)
 
 		// decoded, _ :=bencode.Decode(strings.NewReader(string(responseData)))
 		var resp TrackerResponse
 		bencode.Unmarshal(strings.NewReader(string(responseData)), &resp)
 		peersBytes := []byte(resp.Peers)
-		for i := 0 ; i<len(peersBytes); i += 6 {
-			ip:= net.IP(peersBytes[i:i+4])
-			port := binary.BigEndian.Uint16(peersBytes[i+4:i+6])
+		for i := 0; i < len(peersBytes); i += 6 {
+			ip := net.IP(peersBytes[i : i+4])
+			port := binary.BigEndian.Uint16(peersBytes[i+4 : i+6])
 			fmt.Printf("%s:%d\n", ip, port)
 		}
 
+	case "handshake":
+		filename := os.Args[2]
+		peer := os.Args[3]
+		torrent, _ := buildTorrent(filename)
+
+		conn, err := net.Dial("tcp", peer)
+		if err != nil {
+			fmt.Println("Error connecting to peer")
+			os.Exit(1)
+		}
+		var buf []byte
+		buf = append(buf, 19)
+		buf = append(buf, []byte("BitTorrent protocol")...)
+		buf = append(buf, make([]byte, 8)...)
+		buf = append(buf, torrent.InfoHash[:]...)
+		buf = append(buf, []byte("00112233445566778899")...)
+		_, err = conn.Write(buf)
+		if err != nil {
+			fmt.Println("Error on protocol handshake", err)
+			os.Exit(1)
+		}
+
+		answer := make([]byte, 68)
+		io.ReadFull(conn, answer)
+
+		// Read last 20 bytes (peerID)
+		fmt.Printf("Peer ID: %x\n", answer[48:])
 
 	default:
 		fmt.Println("Unknown command: " + command)
